7028213275f772e4a1c6e5a2777a540102782b44
Christoph Stettner <christoph.stettner@t-online.de>
2013-06-24 09:07:36 +0200

    extends init function and finishes print function

--- symbol_table.cpp
+++ symbol_table.cpp
@@ -4,34 +4,73 @@
 
 SymbolTable::SymbolTable()
 {
-	level = 0;
+	level = -1;
+	max_number = 0;
 }
 
 void SymbolTable::level_up()
 {
 	level++;
+	//print();
 }
 
 void SymbolTable::level_down()
 {
+	print();
 	level--;
 }
 
 void SymbolTable::insert(string key, int type)
 {
-	symbol_table.push_back(map<string, SymbolTableEntry>());
-	symbol_table[level].insert(pair<string, SymbolTableEntry>(key, SymbolTableEntry(type)));
+	// Vector vergrößern, falls zu klein
+	if(symbol_table.size() <= level) {
+		symbol_table.push_back(map<string, SymbolTableEntry>());
+	}
+
+	// only insert if key not inserted yet in this level
+	if(symbol_table[level].find(key) == symbol_table[level].end()) {
+		// calculate running number
+		int number = symbol_table[level].size();
+		// update max_number for print
+		if(number > max_number) max_number = number;
+		// Insert Element
+		symbol_table[level].insert(pair<string, SymbolTableEntry>(key, SymbolTableEntry(type, number)));
+	}
 }
 
-void SymbolTable::lookup()
+int SymbolTable::lookup(string key, int type, int& level, int& number)
 {
+	if(symbol_table[level].find(key) != symbol_table[level].end()) {
+		return 0;
+	}
 }
 
 void SymbolTable::print()
 {
+	cout << "Start - Print\n";
 	for(int i = 0; i < symbol_table.size(); i++) {
-		for(map_iter iter = symbol_table[i].begin(); iter != symbol_table[i].end(); iter++) {
-			//cout << iterator->
+		for(map_iter iter = symbol_table[i].rbegin(); iter != symbol_table[i].rend(); iter++) {
+			cout << "Ebene: " << i << "\t";
+			cout << "Nummer: " << iter->second.getNumber() << "\t";
+			cout << "Bez.: " << iter->first << "\t";
+			cout << "Typ: " << iter->second.getType() << "\n";
 		}
 	}
+	
+/*
+	for(int i = 0; i < symbol_table.size(); i++) {
+		cout << max_number - i << "\t";
+		if(symbol_table[i].size() >= max_number - i)	{
+			for(map_iter iter = symbol_table[i].rbegin(); iter != symbol_table[i].rend(); iter++) {
+				//cout << "Ebene: " << i << "\t";
+				//cout << "Nummer: " << iter->second.getNumber() << "\t";
+				cout << "Bez.: " << iter->first << " - ";
+				cout << "Typ: " << iter->second.getType() << "\n";
+			}
+		}
+	}
+	cout << "Level\t";
+	cout << "\n";
+*/
+	cout << "End - Print\n";
 }
--- symbol_table.h
+++ symbol_table.h
@@ -1,6 +1,10 @@
 #ifndef SYMBOL_TABLE_H
 #define SYMBOL_TABLE_H
 
+#define _VAR 1
+#define _CONST 2
+#define _PROC 3
+
 #include "symbol_table_entry.h"
 #include <vector>
 #include <map>
@@ -13,14 +17,15 @@ class SymbolTable
 		SymbolTable();
 		void level_up();
 		void level_down();
-		void insert(string key, int type);
-		void lookup();
+		void insert(string, int);
+		int lookup(string, int, int&, int&);
 		void print();
 
 	private:
 		int level;
 		vector< map<string, SymbolTableEntry> > symbol_table;
-		typedef map<string, SymbolTableEntry>::iterator map_iter;
+		typedef map<string, SymbolTableEntry>::reverse_iterator map_iter;
+		int max_number;
 };
 
 #endif
