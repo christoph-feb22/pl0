24b36b64adbd3d0647b8ce97cbd6765cce1f480a
Christoph Stettner <christoph.stettner@softwareinmotion.de>
2013-08-12 15:17:22 +0200

    fixes bug

--- ast/src/ast_assignment_node.cpp
+++ ast/src/ast_assignment_node.cpp
@@ -1,6 +1,6 @@
 #include "../include/ast_assignment_node.h"
 
-ASTAssignmentNode(int delta, int number, ASTNumericExpressionNode * exp) : delta(delta), number(number), expression(exp) {}
+ASTAssignmentNode::ASTAssignmentNode(int delta, int number, ASTNumericExpressionNode * exp) : delta(delta), number(number), expression(exp) {}
 
 void ASTAssignmentNode::execute() {
   memory->insertValue(delta, number, expression->eval());
