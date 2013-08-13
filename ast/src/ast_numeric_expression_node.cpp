424705bd46aaadaea902328a51573aa05d862842
Christoph Stettner <christoph.stettner@softwareinmotion.de>
2013-08-13 14:20:25 +0200

    adds implementation for eval in numeric expression node

--- ast/src/ast_numeric_expression_node.cpp
+++ ast/src/ast_numeric_expression_node.cpp
@@ -1,11 +1,20 @@
 #include "../include/ast_numeric_expression_node.h"
 
+ASTNumericExpressionNode::ASTNumericExpressionNode(ASTTermNode * term) {
+  terms = new TermList;
+  terms->push_back(term);
+}
+
+void ASTNumericExpressionNode::insert(ASTTermNode * term) {
+  terms->push_back(term);
+}
+
 int ASTNumericExpressionNode::eval() {
 	int val = 0;
 
-	// for(TermList::iterator it = terms->begin(); it != terms->end(); ++it) {
-	// 	val += (*it)->eval();
-	// }
+	for(TermList::iterator it = terms->begin(); it != terms->end(); ++it) {
+		val += (*it)->eval();
+	}
 
 	return val;
 }
