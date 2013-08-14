422fef5561a84f92044d8930c49c3cfd45e26e93
Christoph Stettner <christoph.stettner@softwareinmotion.de>
2013-08-14 15:58:17 +0200

    refactors statement block node to be more robust

--- ast/src/ast_statement_block_node.cpp
+++ ast/src/ast_statement_block_node.cpp
@@ -3,8 +3,11 @@
 ASTStatementBlockNode::ASTStatementBlockNode(StatementList * statements) : statements(statements) {}
 
 void ASTStatementBlockNode::execute() {
-  for(StatementList::iterator it = statements->begin(); it != statements->end(); ++it) {
-    (*it)->execute();
+  if(statements) {
+    for(int i = 0; i < statements->size(); i++) {
+      if(statements->at(i))
+      statements->at(i)->execute();
+    }
   }
 }
 
