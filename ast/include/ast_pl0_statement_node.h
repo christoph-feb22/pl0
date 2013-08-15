2685022e8c72bc9f37e3266b18748d4109d2acdf
Christoph Stettner <christoph.stettner@softwareinmotion.de>
2013-08-15 16:24:19 +0200

    adds memory to pl0 statement nodes

--- ast/include/ast_pl0_statement_node.h
+++ ast/include/ast_pl0_statement_node.h
@@ -2,8 +2,12 @@
 #define AST_PL0_STATEMENT_NODE_H
 
 #include "ast_statement_node.h"
+#include "../../memory_management/include/memory_management.h"
 
 class ASTPL0StatementNode : public ASTStatementNode {
+public:
+  ASTPL0StatementNode();
+  ASTPL0StatementNode(MemoryManagement * m);
 };
 
 #endif
