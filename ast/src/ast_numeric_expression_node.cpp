1e3868842e0293c06cc266a69b5460cf9dda8390
Christoph Stettner <christoph.stettner@softwareinmotion.de>
2013-08-18 19:37:59 +0200

    adds correct handling for addition and subtraction

--- ast/src/ast_numeric_expression_node.cpp
+++ ast/src/ast_numeric_expression_node.cpp
@@ -13,7 +13,13 @@ int ASTNumericExpressionNode::eval() {
 	int val = 0;
 
 	for(TermList::iterator it = terms->begin(); it != terms->end(); ++it) {
-		val += (*it)->eval();
+    int op = (*it)->getOperator();
+		if(op == ADD_OP) {
+      val += (*it)->eval();
+    }
+    else if(op == SUB_OP) {
+      val -= (*it)->eval();
+    }
 	}
 
 	return val;
